6668bf4b2d790df1425bb55ff100b322e634f495
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-11-05 00:23:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// File: src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,16 +6,17 @@ import { HelmetProvider } from 'react-helmet-async';
 import { UserProvider } from './context/UserContext';
 import App from './App';
 import './index.css';
+import './i18n'; // Initialize translations
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <UserProvider>
-        <BrowserRouter>
+      <BrowserRouter>
+        <UserProvider>
           <App />
-        </BrowserRouter>
-      </UserProvider>
+        </UserProvider>
+      </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
