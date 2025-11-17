4baeaf39c586d900d88272d100023c2de1a9ce8e
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-11-17 16:02:58 +0100

    Create Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+// src/components/Logo.jsx
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <img src="/logo.png" alt="App Logo" style={{ height: '40px' }} />
+  );
+};
+
+export default Logo;
