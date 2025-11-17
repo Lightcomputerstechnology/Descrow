abe8ebb59ac37bd11e7aa84b6236a40b9fe083b2
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-11-17 13:33:51 +0100

    Update bankAccount.routes.js

--- backend/routes/bankAccount.routes.js
+++ backend/routes/bankAccount.routes.js
@@ -2,25 +2,25 @@
 
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/auth.middleware');
+const { authenticate } = require('../middleware/auth.middleware');
 const bankAccountController = require('../controllers/bankAccount.controller');
 
 // Get list of banks
-router.get('/banks', authMiddleware, bankAccountController.getBanks);
+router.get('/banks', authenticate, bankAccountController.getBanks);
 
 // Add bank account
-router.post('/add', authMiddleware, bankAccountController.addBankAccount);
+router.post('/add', authenticate, bankAccountController.addBankAccount);
 
 // Get user's bank accounts
-router.get('/list', authMiddleware, bankAccountController.getBankAccounts);
+router.get('/list', authenticate, bankAccountController.getBankAccounts);
 
 // Set primary account
-router.put('/primary/:accountId', authMiddleware, bankAccountController.setPrimaryAccount);
+router.put('/primary/:accountId', authenticate, bankAccountController.setPrimaryAccount);
 
 // Delete bank account
-router.delete('/:accountId', authMiddleware, bankAccountController.deleteBankAccount);
+router.delete('/:accountId', authenticate, bankAccountController.deleteBankAccount);
 
 // Initiate payout
-router.post('/payout', authMiddleware, bankAccountController.initiatePayout);
+router.post('/payout', authenticate, bankAccountController.initiatePayout);
 
 module.exports = router;
