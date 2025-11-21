85a38a1559ed99dd322ef563e69e6b7a42e6748d
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-11-21 03:04:48 +0100

    Update ProfilePage.jsx

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -15,7 +15,7 @@ import ProfileTab from './ProfileTab';
 import KYCTab from './KYCTab';
 import SecurityTab from './SecurityTab';
 import SettingsTab from './SettingsTab';
-import BankAccountTab from '../../components/Profile/BankAccountTab'; // ✅ FIXED IMPORT PATH
+import BankAccountTab from '@components/Profile/BankAccountTab';
 import profileService from 'services/profileService';
 import { authService } from 'services/authService';
 import toast from 'react-hot-toast';
